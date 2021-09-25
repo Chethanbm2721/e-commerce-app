39d31735e246914d20ac9dde5a7f50028bb91670
Chethanbm2721 <chethan2721@gmail.com>
2021-09-25 20:56:26 +0530

    imported db and created navbar and home,category,checkout page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
 import './App.css';
+import Navbar from './components/Navbar';
+import Category from './pages/Category';
+import Checkout from './pages/Checkout';
+import Home from './pages/Home';
 
 function App() {
-  return <div>hey</div>;
+  return (
+    <>
+    <Navbar />
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+
+      <Route exact path="/category/:id">
+        <Category />
+      </Route>
+
+      <Route exact path="/checkout">
+        <Checkout />
+      </Route>
+    </Switch>
+    </>
+  )
 }
 
 export default App;
