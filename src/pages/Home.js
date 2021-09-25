7998d65daaa011cf3d8c47b4edb8cca8785e9e75
Chethanbm2721 <chethan2721@gmail.com>
2021-09-25 22:44:29 +0530

    created link to category page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import categories from '../db/categories.json'
 
 const Home = () => {
   return (
     <div>
-      Home
+    <div>
+      {categories.map(c=> (
+        <div key={c.id}>
+          <Link to={`/category/${c.id}`}>{c.name}</Link>
+
+        </div>
+      ))}
+    </div>
     </div>
   )
 }
