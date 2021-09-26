870b6b1ede915e31590f6c78480158f2d301ebdb
Chethanbm2721 <chethan2721@gmail.com>
2021-09-26 16:45:26 +0530

    applied filters to the products

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { useParams } from 'react-router-dom';
+import FilterCheckbox from '../components/FilterCheckbox';
+import Products from '../components/Products';
 import categories from '../db/categories.json'
 import fakeProducts from '../db/products.json'
+import { useFilters } from '../lib/useFilters';
+
+function getComputedProducts(products,filters){
+let result=[...products];
+
+if(filters.delivery){
+  result=result.filter(p=>p.delivery===true);
+}
+if(filters.inStock){
+  result=result.filter(p=>p.inStock===true);
+}
+if(filters.expensive){
+  result=result.filter(p=>p.price>100);
+}
+return result;
+}
 
 const Category = () => {
   const {id}=useParams();
@@ -11,37 +29,53 @@ const Category = () => {
 
   const [products]=useState(fakeProducts.filter(p=>p.categoryId===id));
 
+  const [filter,dispatchFilter]=useFilters({
+    delivery:false,
+    inStock:false,
+    expensive:false,
+  })
+   const filteredProducts=getComputedProducts(products,filter)
+
+   const OnCheckboxChange=useCallback(ev=>{
+     const checkbox=ev.target;
+
+     dispatchFilter({
+       type:'SET',
+       filterName:checkbox.name,
+       value:checkbox.checked,
+     })
+   },[dispatchFilter]);
+
   return (
     <div>
       <div>
         <h3>Filters</h3>
-
-      <div>
-      <input type="checkbox" id="delivery" name="delivery" checked />
-      <label htmlFor="delivery">Delivery</label>
-      </div>
+      <FilterCheckbox
+      id="delivery"
+      name="delivery"
+      checked={filter.delivery}
+      onChange={OnCheckboxChange}
+      label="Delivery"
+      />
+      <FilterCheckbox
+      id="inStock"
+      name="inStock"
+      checked={filter.inStock}
+      onChange={OnCheckboxChange}
+      label="In stock only"
+      />
+      <FilterCheckbox
+      id="expensive"
+      name="expensive"
+      checked={filter.expensive}
+      onChange={OnCheckboxChange}
+      label="Expensive (100+USD)"
+      />
     </div>
     <div>
       <h3>{categoryName}</h3>
       <div>
-        {products.map(({currency,delivery,inStock,name,price,thumbnail,...restOfProduct})=> (
-          <div key={restOfProduct.id}>
-            <img src={thumbnail} alt={name} width={50} />
-            <div>{name}</div>
-
-            <div>
-              {currency} {price}
-              </div>
-
-              <div>{inStock?'In stock':'Out of stock'}</div>
-
-              {delivery && <div>Delivery available</div>}
-              <button type="button" disabled={!inStock}>
-                Add to cart
-              </button>
-
-          </div>
-        ))}
+        <Products products={filteredProducts} />
       </div>
     </div>
     </div>
